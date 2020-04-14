883893942107993a4faf30963165e2e7e8acdf6f
Chantz Baudoux <cbaud001@plattsburgh.edu>
2020-04-13 22:57:36 -0400

    change account UI

--- frontend/gui/src/containers/AccountView.js
+++ frontend/gui/src/containers/AccountView.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import Account from '../components/Account';
 import axios from 'axios';
-import { Card } from 'antd';
+import { Statistic, Row, Col, Button } from 'antd';
 
 const listData = [];
 for (let i = 0; i < 23; i++) {
@@ -34,10 +34,14 @@ class AccountList extends React.Component{
     render() {
         const { account } = this.state;
         return(
-            <div className="containter">
-                <h1 className="center">equity</h1>
-                { account.buying_power }
-            </div>
+        <Row gutter={16}>
+         <Col span={12}>
+         <Statistic title="Portfolio Value" value={ account.equity } precision={2} />
+         <Button style={{ marginTop: 16 }} type="primary">
+             Recharge
+         </Button>
+         </Col>
+         </Row>
         )
     }
 }
