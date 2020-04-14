eb21d4b2349746c239cd3e5e3ba58807b4645a04
Ryan Pinnock <ryanpinnock10@gmail.com>
2020-04-14 15:11:55 -0400

    getOrder attempt + Stock chart + App.js

--- frontend/gui/src/containers/getOrderHistory.js
+++ frontend/gui/src/containers/getOrderHistory.js
@@ -1,57 +1,51 @@
-import React from 'react';
-import axios from 'axios';
-import { Table } from 'antd';
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Table } from 'antd';
+const { Column, ColumnGroup } = Table;
 
-// const listData = [];
-// for (let i = 0; i < 23; i++) {
-//   listData.push({
-//     title: `ant design part ${i}`,
-//     quantity: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-//     bought_price:
-//       'Ant Design, a design language for background applications, is refined by Ant UED Team.',
-//     current_price:
-//       'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
-//   });
-// }
-
-class OrderList extends React.Component{
-//go through and clean up code.. positions here should be different variable
-    state = {
-        positions: []
+class OrderList extends Component {
+//go through and clean up code.. positions here should be different variable
+    constructor(props)
+    {
+        super(props);
+        this.state = {
+        userOrders: []
+        }
     }
 
-    componentDidMount() {
-        axios.get('http://127.0.0.1:8000/api/order_history/')
-            .then(res => {
-                this.setState({
-                    positions: res.data
-                });
-                console.log(res.dat)
-            })
-    }
+    componentDidMount() {
+        axios.get('http://127.0.0.1:8000/api/order_history/')
+            .then(res => {
+                this.setState({
+                    userOrders: res.data
+                });
+                console.log(res.data)
+            })
+    }
 
-    render() {
-            // <Positions data = {this.state.positions}/>
-            const { positions: orders } = this.state;
-            const order_List = orders.length ? (
-                orders.map(positions => {
-                    return (
-                        <Table> 
-                            
-                        </Table>
-                    )
-                })
-            ):(
-                <div className="center">No posts yet. Remember to run the backend server</div>
-            )
-        return (
-            <div className="container">
-                <h1 className="center">Portfolio</h1>
-                { order_List }
-            </div>
-        )
-        
-    }
+    render() {
+            const { userOrders } = this.state;
+            const order_List = userOrders.length ? (
+                userOrders.map(userOrders => {
+                    return (
+                        <Table> 
+                            <ColumnGroup title="Order History">
+                            <Column title="Symbol" dataIndex=  "symbol" key = "symbol"/>
+                            <Column title="Avg Cost" dataIndex= "price" key =  "price"/>
+                            </ColumnGroup>
+                        </Table> 
+                    )
+                })
+            ):(
+                <div className="center">No posts yet. Remember to run the backend server</div>
+            )
+        return (
+            <div className="container">
+                <h1 className="center">Order History</h1>
+                { order_List }
+            </div>
+        )
+    }
 }
 
-export default OrderList;
+export default OrderList;
