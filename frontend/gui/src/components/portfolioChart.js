62ed9af14edb1fbf1b2b40da04c4917efb8a0ea9
Ryan Pinnock <ryanpinnock10@gmail.com>
2020-04-14 16:41:23 -0400

    plot trial 1- success

--- frontend/gui/src/components/portfolioChart.js
+++ frontend/gui/src/components/portfolioChart.js
@@ -1,73 +1,73 @@
-// import React from 'react';
-// import Plot from 'react-plotly.js';
+import React from 'react';
+import Plot from 'react-plotly.js';
 
-// class Stock extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       stockChartXValues: [],
-//       stockChartYValues: []
-//     }
-//   }
+class Stock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      stockChartXValues: [],
+      stockChartYValues: []
+    }
+  }
 
-//   componentDidMount() {
-//     this.fetchStock();
-//   }
+  componentDidMount() {
+    this.fetchStock();
+  }
 
-//   fetchStock() {
-//     const pointerToThis = this;
-//     console.log(pointerToThis);
-//     const API_KEY = 'HGJWFG4N8AQ66ICD';
-//     let StockSymbol = 'FB';
-//     let API_Call = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${StockSymbol}&outputsize=compact&apikey=${API_KEY}`;
-//     let stockChartXValuesFunction = [];
-//     let stockChartYValuesFunction = [];
+  fetchStock() {
+    const pointerToThis = this;
+    console.log(pointerToThis);
+    const API_KEY = 'HGJWFG4N8AQ66ICD';
+    let StockSymbol = 'AMZN';
+    let API_Call = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${StockSymbol}&outputsize=compact&apikey=${API_KEY}`;
+    let stockChartXValuesFunction = [];
+    let stockChartYValuesFunction = [];
 
-//     fetch(API_Call)
-//       .then(
-//         function(response) {
-//           return response.json();
-//         }
-//       )
-//       .then(
-//         function(data) {
-//           console.log(data);
+    fetch(API_Call)
+      .then(
+        function(response) {
+          return response.json();
+        }
+      )
+      .then(
+        function(data) {
+          console.log(data);
 
-//           for (var key in data['Time Series (Daily)']) {
-//             stockChartXValuesFunction.push(key);
-//             stockChartYValuesFunction.push(data['Time Series (Daily)'][key]['1. open']);
-//           }
+          for (var key in data['Time Series (Daily)']) {
+            stockChartXValuesFunction.push(key);
+            stockChartYValuesFunction.push(data['Time Series (Daily)'][key]['1. open']);
+          }
 
-//           // console.log(stockChartXValuesFunction);
-//           pointerToThis.setState({
-//             stockChartXValues: stockChartXValuesFunction,
-//             stockChartYValues: stockChartYValuesFunction
-//           });
-//         }
-//       )
-//   }
+          // console.log(stockChartXValuesFunction);
+          pointerToThis.setState({
+            stockChartXValues: stockChartXValuesFunction,
+            stockChartYValues: stockChartYValuesFunction
+          });
+        }
+      )
+  }
 
-//   render() {
-//     return (
-//       <div id='Stock Plot'>
-//         <h1>Landing</h1>
-//         <h2>Stock Market</h2>
-//         <Plot
-//           data={[
-//             {
-//               x: this.state.stockChartXValues,
-//               y: this.state.stockChartYValues,
-//               type: 'bar',
-//               mode: 'lines+markers',
-//               marker: {color: 'blue'},
-//               //showlegend: false,
-//             }
-//           ]}
-//           layout={{width: 720, height: 440, responsive: true, showlegend: false,title: 'Stock Plot'}}
-//         />
-//       </div>
-//     )
-//   }
-// }
+  render() {
+    return (
+      <div id='Stock Plot'>
+        <h1>Landing</h1>
+        <h2>Stock Market</h2>
+        <Plot
+          data={[
+            {
+              x: this.state.stockChartXValues,
+              y: this.state.stockChartYValues,
+              type: 'line',
+              mode: 'lines+markers',
+              marker: {color: 'blue'},
+              //showlegend: false,
+            }
+          ]}
+          layout={{width: 720, height: 440, responsive: true, showlegend: false,title: 'Stock Plot'}}
+        />
+      </div>
+    )
+  }
+}
 
-// export default Stock;
+export default Stock;
