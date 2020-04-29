61f2f57535aa988070f440ca887836663970d83f
Ryan Pinnock <ryanpinnock10@gmail.com>
2020-04-29 08:17:33 -0400

    minor

--- frontend/gui/src/components/watchlist.js
+++ frontend/gui/src/components/watchlist.js
@@ -56,7 +56,7 @@ class WatchList extends React.Component{
         return(
             <div style={{float:"right"}}>
             <h2>Watch List</h2>
-            <Table style={{width:150, float:"right", paddingRight:200}} 
+            <Table style={{width:150, float:"right", paddingRight:250}} 
                 bordered="true"
                 class="watchlist" 
                 columns={columns} 
